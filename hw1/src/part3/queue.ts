5a05a135f6e7277c9a72fbebad12564ff693d589
Ksenia Drokov <kseniadrokov@Ksenias-MacBook-Pro.local>
2021-03-21 17:47:15 +0200

    Queue & State

--- hw1/src/part3/queue.ts
+++ hw1/src/part3/queue.ts
@@ -7,9 +7,13 @@ export const enqueue = (x: number): State<Queue,undefined> => {
     return initState => [initState.concat([x]), undefined];
 };
 
-export const dequeue = undefined;
+export const dequeue = (): State<Queue, number> => {
+    return initState => [initState.slice(1),initState[0]];
+};
 
-export const queueManip = undefined;
+export const queueManip = (): State<Queue, number> => {
+    return bind(dequeue(), x => bind(enqueue(x*2), y => bind(enqueue(x/3), z => dequeue())));
+};
 
 let add5 = enqueue(5);
 console.log(add5([1,2,3,4]));
--- hw1/src/part3/state.ts
+++ hw1/src/part3/state.ts
@@ -1,4 +1,9 @@
 export type State<S, A> = (initialState: S) => [S, A];
 
-export const bind = undefined;
-//<S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>) => State<S, B> = undefined;
+export const bind: <S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>) => State<S, B> = (state, f) => {
+    return initState => {
+        const [newState, value] = state(initState);
+        const g = f(value);
+        return g(newState);
+    }
+};
